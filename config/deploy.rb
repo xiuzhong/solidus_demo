48395458d4b31363f6d6e28711b38b9a389177a1
Leo Li <lxz.tty@gmail.com>
2020-09-20 08:11:03 +1000

    ask branch to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:xiuzhong/solidus_demo.git"
 set :user, 'ec2-user'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/#{fetch(:user)}/nextbig"
