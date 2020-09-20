30aef0a8a0f2c8cfadc597f6c33284a063b0a136
Leo Li <lxz.tty@gmail.com>
2020-09-20 10:31:08 +1000

    fix puma restart

--- Capfile
+++ Capfile
@@ -30,6 +30,7 @@ require "capistrano/rvm"
 # require "capistrano/rbenv"
 # require "capistrano/chruby"
 require "capistrano/bundler"
+require 'capistrano/rails'
 require "capistrano/rails/assets"
 require "capistrano/rails/migrations"
 require 'capistrano/puma'
--- config/deploy.rb
+++ config/deploy.rb
@@ -43,4 +43,8 @@ set :rails_env, :production
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :puma_init_active_record, true
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{shared_path}/log/puma.error.log"
+set :puma_error_log,  "#{shared_path}/log/puma.access.log"
 set :puma_conf, -> { File.join(current_path, 'config', 'puma.rb') }
