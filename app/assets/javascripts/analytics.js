29175da3414aebafbdcdf252ba0b537922ed0064
Qi Wang <bladewangqi@gmail.com>
2020-09-25 11:15:03 +1000

    setup gtm demo

---
+++ app/assets/javascripts/analytics.js
@@ -0,0 +1,12 @@
+// analytics.js
+// The placeholder for playing around the analytics.
+// If you think your code is not called, uncomment the below one and see if `Next big thing` pops up
+// alert('Next big thing !!!')
+console.log("===========> ANALYTICS playground");
+// Google Tag Manager -->
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PZRRDN4');
+// End Google Tag Manager -->
