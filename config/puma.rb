41fec4fc7ceb2a5f87d26075a6f98f300ff1d67f
Leo Li <lxz.tty@gmail.com>
2020-09-26 07:23:09 +1000

    make puma.rb for development

--- config/puma.rb
+++ config/puma.rb
@@ -1,27 +1,34 @@
-#!/usr/bin/env puma
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
 
-directory '/home/ec2-user/nextbig/current'
-rackup "/home/ec2-user/nextbig/current/config.ru"
-environment 'production'
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port        ENV.fetch("PORT") { 4000 }
 
-tag ''
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-pidfile "/home/ec2-user/nextbig/shared/tmp/pids/puma.pid"
-state_path "/home/ec2-user/nextbig/shared/tmp/pids/puma.state"
-stdout_redirect '/home/ec2-user/nextbig/shared/log/puma.error.log', '/home/ec2-user/nextbig/shared/log/puma.access.log', true
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked webserver processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+# preload_app!
 
-threads 4,16
-
-bind 'unix:///home/ec2-user/nextbig/shared/tmp/sockets/puma.sock'
-
-workers 0
-
-restart_command 'bundle exec --keep-file-descriptors puma'
-
-prune_bundler
-
-on_restart do
-  puts 'Refreshing Gemfile'
-  ENV["BUNDLE_GEMFILE"] = ""
-end
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
